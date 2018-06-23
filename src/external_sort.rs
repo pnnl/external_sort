b5fd5738de3e20feabc3f1d99b07428773e89eb2
Phillips, Aaron R <Aaron.Phillips@pnnl.gov>
2018-06-23 15:24:31 -0700

    Adding examples to doc

--- src/external_sort.rs
+++ src/external_sort.rs
@@ -88,6 +88,44 @@ where
 }
 
 /// Perform an external sort on an unsorted stream of incoming data
+/// 
+/// # Examples 
+/// 
+/// ```
+/// extern crate external_sort;
+/// #[macro_use]
+/// extern crate serde_derive;
+/// 
+/// use external_sort::{ExternallySortable, ExternalSorter};
+///
+/// #[derive(Serialize, Deserialize, Clone, PartialEq, Eq, PartialOrd, Ord)]
+/// struct Num {
+///     the_num: u32
+/// }
+///
+/// impl Num {
+///     fn new(num:u32) -> Num {
+///         Num { the_num: num }
+///     }
+/// }
+///
+/// impl ExternallySortable for Num {
+///     fn get_size(&self) -> u64 {
+///         4
+///     }
+/// }
+///
+/// fn main() {
+///     let unsorted = vec![Num::new(5), Num::new(2), Num::new(1), Num::new(3), Num::new(4)];
+///     let sorted = vec![Num::new(1), Num::new(2), Num::new(3), Num::new(4), Num::new(5)];
+///
+///     let external_sorter = ExternalSorter::new(16, None);
+///     let iter = external_sorter.sort(unsorted.into_iter());
+///     for (idx, i) in iter.enumerate() {
+///         assert_eq!(i.the_num, sorted[idx].the_num);
+///     }
+/// }
+/// ```
 pub struct ExternalSorter<T>
 where
     T: ExternallySortable,
